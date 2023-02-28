b01e01a172c648bb59ebd90cd0ef47eade8f058f
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-02-28 04:03:15 +0200

    named MainActicity by HomeActivity

--- app/src/main/java/com/example/textdetector/SplashScreen.kt
+++ app/src/main/java/com/example/textdetector/SplashScreen.kt
@@ -17,7 +17,7 @@ class SplashScreen : AppCompatActivity() {
     }
 
     fun starthomeactivity() {
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
         finish()
 
