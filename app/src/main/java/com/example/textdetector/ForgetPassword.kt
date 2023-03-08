4ebd4ba590f66667e1e0f8dec35041c7ece139ab
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-03-08 14:44:29 +0200

    add forgetPassword and Register pages

--- app/src/main/java/com/example/textdetector/ForgetPassword.kt
+++ app/src/main/java/com/example/textdetector/ForgetPassword.kt
@@ -9,13 +9,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.textdetector.databinding.ActivityForgetPasswordBinding
 
-class ForgetPasswordActivity : AppCompatActivity() {
+class ForgetPassword : AppCompatActivity() {
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_forget_password)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
 
--- app/src/main/java/com/example/textdetector/LoginActivity.kt
+++ app/src/main/java/com/example/textdetector/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.example.textdetector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var signupTextview:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signupTextview=findViewById(R.id.SignUp)
+
+
+
+
     }
 }
