5e7d0a605249226f1cf12028faca26d21d49a604
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-03-31 18:40:20 +0200

    adding Facebook integration development Mode

--- app/src/main/java/com/example/textdetector/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/textdetector/ui/login/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,12 +20,24 @@ import com.example.textdetector.R;
 import com.example.textdetector.ui.forgetpassword.ForgetPassword;
 import com.example.textdetector.ui.home.HomeActivity;
 import com.example.textdetector.ui.signup.CreateAccount;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,14 +46,18 @@ public class LoginActivity extends AppCompatActivity {
     TextView signUpBtn;
     TextView forgetPasswordBtn;
     Button loginBtn;
-    EditText inputEmail, inputPassword ;
+    EditText inputEmail, inputPassword;
     private FirebaseAuth mAuth;
     public static String username;
+    ImageView fbBtn;
+    CallbackManager callbackManager;
+    AccessToken accessToken;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
 
@@ -49,7 +66,18 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.login_Button);
         inputEmail = (EditText) findViewById(R.id.et_email_login);
         inputPassword = (EditText) findViewById(R.id.et_password_login);
+        fbBtn = findViewById(R.id.facebook_logo);
+        fbBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile"));
+
+            }
+        });
+
+        facebookSignIn();
         mAuth = FirebaseAuth.getInstance();
         // Hide password toggle icon by default
         TextInputLayout passwordInput = findViewById(R.id.Password_Input);
@@ -121,6 +149,62 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Onseccess();
+        }
+    }
+
+    void facebookSignIn() {
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        accessToken = AccessToken.getCurrentAccessToken();
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                accessToken,
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(
+                                            JSONObject object,
+                                            GraphResponse response) {
+//                                            personFullName = object.getString("name");
+//                                            personId = object.getString("id");
+//                                            personFirstName = object.getString("first_name");
+//                                            personLastName = object.getString("last_name");
+//                                            personEmail = object.getString("email");
+//                                            personPhoto = object.getJSONObject("picture").getJSONObject("data").getString("url");
+                                        Onseccess();
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,first_name,last_name,email,picture.type(large)");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    private void Onseccess() {
+        LoginToast();
+        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(i);
+    }
+
     private Boolean checkCredentials() {
         Boolean isVaild = true;
         String email = inputEmail.getText().toString();
@@ -154,8 +238,4 @@ public class LoginActivity extends AppCompatActivity {
         toast.show();
     }
 
-
-
 }
-
-
