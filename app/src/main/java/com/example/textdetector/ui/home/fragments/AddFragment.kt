7a14b5834565456ce41a17aa364c3649fd2997a7
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-04-24 03:25:12 +0200

    adding flask website Api by retrofit

--- app/src/main/java/com/example/textdetector/ui/home/fragments/AddFragment.kt
+++ app/src/main/java/com/example/textdetector/ui/home/fragments/AddFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.textdetector.R
 import com.example.textdetector.ui.home.fragments.ResultSplashFragment
 import com.example.textdetector.ui.home.fragments.api.ApiInterface
@@ -17,68 +18,32 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class AddFragment : Fragment() {
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-
-            /*private val client = OkHttpClient.Builder().build()
-
-            private val retrofit = Retrofit.Builder()
-                .baseUrl("http://127.0.0.1:5000/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-
-            fun<T> buildService(service: Class<T>): T{
-                return retrofit.create(service)
-            }*/
-        /*val response = ApiService.buildService(ApiInterface::class.java)
-        response.sendReq(requestModel).enqueue(
-            object : Callback<ResponseModel> {
-                override fun onResponse(
-                    call: Call<ResponseModel>,
-                    response: Response<ResponseModel>
-                ) {
-                    Toast.makeText(this@MainActivity,response.message().toString(),Toast.LENGTH_LONG).show()
-                }
-
-                override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
-                    Toast.makeText(this@MainActivity,t.toString(),Toast.LENGTH_LONG).show()
-                }
-
-            }
-        )*/
-
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_add, container, false)
 
         val etTypeTweet = view.findViewById<EditText>(R.id.et_TypeTweet)
         val showResultsButton = view.findViewById<Button>(R.id.showResults_button)
         showResultsButton.setOnClickListener {
-
-            val resultSplashFragment = ResultSplashFragment()
-
-
-            val fragmentManager = requireActivity().supportFragmentManager
-            fragmentManager.beginTransaction()
-                .replace(R.id.fragment_contanier, resultSplashFragment)
-                .addToBackStack(null)
-                .commit()
+            val tweet = etTypeTweet.text.toString()
+            val words = tweet.split(" ")
+            if (words.size > 2) {
+                val resultFragment = ResultFragment()
+                val bundle = Bundle()
+                bundle.putString("tweet", tweet)
+                resultFragment.arguments = bundle
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_contanier, resultFragment)
+                    .addToBackStack(null)
+                    .commit()
+            } else {
+                Toast.makeText(requireContext(), "Tweet must have more than 2 words", Toast.LENGTH_SHORT).show()
+            }
         }
 
         return view
     }
-
-
 }
--- app/src/main/java/com/example/textdetector/ui/home/fragments/ResultFragment.kt
+++ app/src/main/java/com/example/textdetector/ui/home/fragments/ResultFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.textdetector.R
 import com.example.textdetector.ui.home.fragments.api.ApiInterface
 import com.example.textdetector.ui.home.fragments.api.PredictionRequest
@@ -21,22 +22,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ResultFragment : Fragment() {
 
+
+
     // Create an instance of the Retrofit class
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://127.0.0.1:8080")
+        .baseUrl("http://10.0.2.2:8080")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     // Create an instance of the API interface
     private val apiInterface = retrofit.create(ApiInterface::class.java)
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,47 +39,60 @@ class ResultFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
-
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val tweet = requireArguments().getString("tweet")
+        Log.d("antsh", "the Tweet: $tweet")
         // Create a PredictionRequest object with the tweet text
-        val requestModel = PredictionRequest("I Love you")
+
+        val requestModel = PredictionRequest("I Love You")
 
         // Make a POST request to the /predict endpoint
         apiInterface.sendReq(requestModel).enqueue(object : Callback<PredictionResponse> {
-            override fun onResponse(call: Call<PredictionResponse>, response: Response<PredictionResponse>) {
+            override fun onResponse(
+                call: Call<PredictionResponse>,
+                response: Response<PredictionResponse>
+            ) {
                 if (response.isSuccessful) {
                     val classLabel = response.body()?.classLabel
                     val offensiveProb = response.body()?.classProbs?.offensive
                     val hateProb = response.body()?.classProbs?.hate
                     val neitherProb = response.body()?.classProbs?.neither
 
+                    val classProbList = listOf(
+                        "Offensive" to offensiveProb,
+                        "Hate" to hateProb,
+                        "Neither" to neitherProb
+                    )
+
+                    val sortedClassProbList = classProbList.sortedByDescending { it.second }
                     Log.d("Prediction", "Class label: $classLabel")
-                    Log.d("Prediction", "Offensive probability: $offensiveProb")
-                    Log.d("Prediction", "Hate probability: $hateProb")
-                    Log.d("Prediction", "Neither probability: $neitherProb")
-                    // Display the prediction results in the UI
-//                    class_label_text_view.text = "Class label: $classLabel"
-//                    offensive_prob_text_view.text = "Offensive probability: $offensiveProb"
-//                    hate_prob_text_view.text = "Hate probability: $hateProb"
-//                    neither_prob_text_view.text = "Neither probability: $neitherProb"
+                    sortedClassProbList.forEach { (label, prob) ->
+                        Log.d("Prediction", "$label probability: $prob")
+                    }
+                    updateLabelTweetText(classLabel)
+                    val max = sortedClassProbList[0].second ?: 0.0
+
+
+
+
                 } else {
                     // Handle the error
-                    Log.e("PredictionError", "Prediction request failed with status code ${response.code()}")
-
+                    Log.e(
+                        "PredictionError",
+                        "Prediction request failed with status code ${response.code()}"
+                    )
                 }
             }
+
             override fun onFailure(call: Call<PredictionResponse>, t: Throwable) {
                 // Handle the failure
                 Log.e("PredictionError", "Prediction request failed with exception", t)
-
             }
         })
-
 
         val progressBar = view.findViewById<CircularProgressIndicator>(R.id.progressbar1)
         val animator = ObjectAnimator.ofInt(progressBar, "progress", 0, 62)
@@ -101,8 +109,20 @@ class ResultFragment : Fragment() {
         animator3.duration = 1000
         animator3.start()
 
+    }
 
+
+    private fun updateLabelTweetText(classLabel: Int?) {
+        val labelTweet = requireView().findViewById<TextView>(R.id.tweet_type)
+
+        when (classLabel) {
+            0 -> labelTweet.text = "HATE TWEET"
+            1 -> labelTweet.text = "OFFENSIVE TWEET"
+            2 -> labelTweet.text = "NEITHER TWEET"
+        }
     }
 
 
 }
+
+
