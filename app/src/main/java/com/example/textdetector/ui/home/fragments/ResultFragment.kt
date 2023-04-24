1e46bfa504d103c84d7c876b690ef3841879e1de
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2023-04-24 07:31:50 +0200

    update

--- app/src/main/java/com/example/textdetector/ui/home/fragments/ResultFragment.kt
+++ app/src/main/java/com/example/textdetector/ui/home/fragments/ResultFragment.kt
@@ -50,134 +50,128 @@ class ResultFragment : Fragment() {
         Log.d("antsh", "the Tweet: $tweet")
         // Create a PredictionRequest object with the tweet text
 
-        val requestModel = PredictionRequest("son of bitch")
+        val requestModel = tweet?.let { PredictionRequest(it) }
 
         // Make a POST request to the /predict endpoint
-        apiInterface.sendReq(requestModel).enqueue(object : Callback<PredictionResponse> {
-            override fun onResponse(
-                call: Call<PredictionResponse>,
-                response: Response<PredictionResponse>
-            ) {
-                if (response.isSuccessful) {
-                    val classLabel = response.body()?.classLabel
-                    val offensiveProb = response.body()?.classProbs?.offensive
-                    val hateProb = response.body()?.classProbs?.hate
-                    val neitherProb = response.body()?.classProbs?.neither
+        if (requestModel != null) {
+            apiInterface.sendReq(requestModel).enqueue(object : Callback<PredictionResponse> {
+                override fun onResponse(
+                    call: Call<PredictionResponse>,
+                    response: Response<PredictionResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val classLabel = response.body()?.classLabel
+                        val offensiveProb = response.body()?.classProbs?.offensive
+                        val hateProb = response.body()?.classProbs?.hate
+                        val neitherProb = response.body()?.classProbs?.neither
 
-                    val classProbList = listOf(
-                        "Offensive" to offensiveProb,
-                        "Hate" to hateProb,
-                        "Neither" to neitherProb
-                    )
+                        val classProbList = listOf(
+                            "Offensive" to offensiveProb,
+                            "Hate" to hateProb,
+                            "Neither" to neitherProb
+                        )
 
-                    val sortedClassProbList = classProbList.sortedByDescending { it.second }
-                    Log.d("Prediction", "Class label: $classLabel")
-                    sortedClassProbList.forEach { (label, prob) ->
-                        Log.d("Prediction", "$label probability: $prob")
+                        val sortedClassProbList = classProbList.sortedByDescending { it.second }
+                        Log.d("Prediction", "Class label: $classLabel")
+                        sortedClassProbList.forEach { (label, prob) ->
+                            Log.d("Prediction", "$label probability: $prob")
+                        }
+                        val classLabel1 = sortedClassProbList[0].first
+                        val classProb1 = sortedClassProbList[0].second
+                        val classLabel2 = sortedClassProbList[1].first
+                        val classProb2 = sortedClassProbList[1].second
+                        val classLabel3 = sortedClassProbList[2].first
+                        val classProb3 = sortedClassProbList[2].second
+
+                        //update the labels and its percentages
+                        val label_one = view.findViewById<TextView>(R.id.first_label)
+                        label_one.text ="${classLabel1} language"
+                        val prob_one = view.findViewById<TextView>(R.id.first_percentage)
+                        prob_one.text ="${classProb1}"
+
+                        val label_two = view.findViewById<TextView>(R.id.second_label)
+                        label_two.text ="${classLabel2} language"
+                        val prob_two = view.findViewById<TextView>(R.id.second_percentage)
+                        prob_two.text ="${classProb2}"
+
+                        val label_three = view.findViewById<TextView>(R.id.third_label)
+                        label_three.text ="${classLabel3} language"
+                        val prob_three = view.findViewById<TextView>(R.id.third_percentage)
+                        prob_three.text ="${classProb3}"
+
+                        /*Log.d("Prediction", "Class label: $classLabel1")
+                        Log.d("Prediction", "Class prob: $classProb1")
+                        Log.d("Prediction", "Class label: $classLabel2")
+                        Log.d("Prediction", "Class prob: $classProb2")
+                        Log.d("Prediction", "Class label: $classLabel3")
+                        Log.d("Prediction", "Class prob: $classProb3")*/
+
+                        updateLabelTweetText(classLabel)
+                        updatePercentages(classProb1,classProb2,classProb3)
+                        val max = sortedClassProbList[0].second ?: 0.0
+                        val percentageTextView = view.findViewById<TextView>(R.id.percentage)
+                        percentageTextView.text = "${max}"
+
+
+
+
+                    } else {
+                        // Handle the error
+                        Log.e(
+                            "PredictionError",
+                            "Prediction request failed with status code ${response.code()}"
+                        )
                     }
-                    val classLabel1 = sortedClassProbList[0].first
-                    val classProb1 = sortedClassProbList[0].second
-                    val classLabel2 = sortedClassProbList[1].first
-                    val classProb2 = sortedClassProbList[1].second
-                    val classLabel3 = sortedClassProbList[2].first
-                    val classProb3 = sortedClassProbList[2].second
-
-                    //update the labels and its percentages
-                    val label_one = view.findViewById<TextView>(R.id.first_label)
-                    label_one.text ="${classLabel1} language"
-                    val prob_one = view.findViewById<TextView>(R.id.first_percentage)
-                    prob_one.text ="${classProb1}"
-
-                    val label_two = view.findViewById<TextView>(R.id.second_label)
-                    label_two.text ="${classLabel2} language"
-                    val prob_two = view.findViewById<TextView>(R.id.second_percentage)
-                    prob_two.text ="${classProb2}"
-
-                    val label_three = view.findViewById<TextView>(R.id.third_label)
-                    label_three.text ="${classLabel3} language"
-                    val prob_three = view.findViewById<TextView>(R.id.third_percentage)
-                    prob_three.text ="${classProb3}"
-
-
-                    Log.d("Prediction", "Class label: $classLabel1")
-                    Log.d("Prediction", "Class prob: $classProb1")
-                    Log.d("Prediction", "Class label: $classLabel2")
-                    Log.d("Prediction", "Class prob: $classProb2")
-                    Log.d("Prediction", "Class label: $classLabel3")
-                    Log.d("Prediction", "Class prob: $classProb3")
-
-                    updateLabelTweetText(classLabel)
-                    updatePercentages(classProb1,classProb2,classProb3)
-                    val max = sortedClassProbList[0].second ?: 0.0
-                    val percentageTextView = view.findViewById<TextView>(R.id.percentage)
-                    percentageTextView.text = "${max}"
-
-
-
-
-                } else {
-                    // Handle the error
-                    Log.e(
-                        "PredictionError",
-                        "Prediction request failed with status code ${response.code()}"
-                    )
                 }
-            }
 
-            override fun onFailure(call: Call<PredictionResponse>, t: Throwable) {
-                // Handle the failure
-                Log.e("PredictionError", "Prediction request failed with exception", t)
-            }
-        })
+                override fun onFailure(call: Call<PredictionResponse>, t: Throwable) {
+                    // Handle the failure
+                    Log.e("PredictionError", "Prediction request failed with exception", t)
+                }
+            })
+        }
 
 
 
     }
+
 
 
 
     fun updatePercentages(classProb1: String?, classProb2: String?, classProb3: String?) {
 
+        var x: Float? = null
+        var y: Float? = null
+        var z: Float? = null
 
-        val progressBar = view?.findViewById<CircularProgressIndicator>(R.id.progressbar1)
-        val animator = ObjectAnimator.ofInt(progressBar, "progress", 0, 90)
-        animator.duration = 1000
-        animator.start()
+        // Parse classProb1, classProb2, and classProb3 as floats, if possible
+        try {
+            x = classProb1?.replace("%", "")?.toFloat()
+            y = classProb2?.replace("%", "")?.toFloat()
+            z = classProb3?.replace("%", "")?.toFloat()
+        } catch (e: NumberFormatException) {
+            Log.e("MyApp", "Error parsing floats from strings: ${e.message}")
+            return  // Exit the function if there was an error
+        }
 
-        val progressBar2 = view?.findViewById<CircularProgressIndicator>(R.id.progressbar2)
-        val animator2 = ObjectAnimator.ofInt(progressBar2, "progress", 0, 50)
-        animator2.duration = 1000
-        animator2.start()
-
-        val progressBar3 = view?.findViewById<CircularProgressIndicator>(R.id.progressbar3)
-        val animator3 = ObjectAnimator.ofInt(progressBar3, "progress", 0, 50)
-        animator3.duration = 1000
-        animator3.start()
-    }
-
-    /*private fun updatePercentages(classProb1: String?, classProb2: String?, classProb3: String?) {
+        // Get references to the progress bars and create animators for each one
         val progressBar1 = view?.findViewById<CircularProgressIndicator>(R.id.progressbar1)
-        val progressPercentage1 = classProb1?.toFloatOrNull() ?: 0f // Desired percentage as float
-        val progressValue1 = (progressPercentage1 / 100.0f) * progressBar1?.max!! // Calculate progress value as float
-        val animator1 = ObjectAnimator.ofFloat(progressBar1, "progress", 0f, progressValue1)
-        animator1.duration = 1000
-        animator1.start()
+        val animator1 = progressBar1?.let { ObjectAnimator.ofInt(it, "progress", 0, (x ?: 0f * 100).toInt()) }
+        animator1?.duration = 1000
 
         val progressBar2 = view?.findViewById<CircularProgressIndicator>(R.id.progressbar2)
-        val progressPercentage2 = classProb2?.toFloatOrNull() ?: 0f // Desired percentage as float
-        val progressValue2 = (progressPercentage2 / 100.0f) * progressBar2?.max!! // Calculate progress value as float
-        val animator2 = ObjectAnimator.ofFloat(progressBar2, "progress", 0f, progressValue2)
-        animator2.duration = 1000
-        animator2.start()
+        val animator2 = progressBar2?.let { ObjectAnimator.ofInt(it, "progress", 0, (y ?: 0f * 100).toInt()) }
+        animator2?.duration = 1000
 
         val progressBar3 = view?.findViewById<CircularProgressIndicator>(R.id.progressbar3)
-        val progressPercentage3 = classProb3?.toFloatOrNull() ?: 0f // Desired percentage as float
-        val progressValue3 = (progressPercentage3 / 100.0f) * progressBar3?.max!! // Calculate progress value as float
-        val animator3 = ObjectAnimator.ofFloat(progressBar3, "progress", 0f, progressValue3)
-        animator3.duration = 1000
-        animator3.start()
-    }*/
+        val animator3 = progressBar3?.let { ObjectAnimator.ofInt(it, "progress", 0, (z ?: 0f * 100).toInt()) }
+        animator3?.duration = 1000
 
+        // Start the animations
+        animator1?.start()
+        animator2?.start()
+        animator3?.start()
+    }
 
     private fun updateLabelTweetText(classLabel: Int?) {
         val labelTweet = requireView().findViewById<TextView>(R.id.tweet_type)
